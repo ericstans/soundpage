7dad96952fdf8d5d03052c91a741e4e7d0452c18
Me <ericrvaa@gmail.com>
2025-08-27 05:49:42 -0400

    update routes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
-import { fileURLToPath, URL } from 'node:url';
-
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/soundpage/' : '/',
+export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
@@ -17,4 +15,4 @@ export default defineConfig(({ mode }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-}));
+})
