126006d41574ade8ebecc170396b4a48cdd35e3f
Me <ericrvaa@gmail.com>
2025-08-27 05:33:50 -0400

    router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './views/Home.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
+  { path: '/soundpage', name: 'Home', component: Home }
 ];
 
 const router = createRouter({
