b8dded03b76a960d064dbbaa66bcfdba9a5c0440
Me <ericrvaa@gmail.com>
2025-08-27 05:59:40 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://ericstans.github.com/soundpage/',
   plugins: [
     vue(),
     vueDevTools(),
