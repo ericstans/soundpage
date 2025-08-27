1e637d187f085fe107dd106531c317d7baded0d4
Me <ericrvaa@gmail.com>
2025-08-27 05:20:56 -0400

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,4 @@
 </template>
 
 <script setup>
-// App shell for SoundCloud clone
 </script>
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-
